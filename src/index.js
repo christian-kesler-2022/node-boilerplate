2b6852657a4e67530c02fd6589d2c8400200c23b
christian-kesler-2022 <111981585+christian-kesler-2022@users.noreply.github.com>
2022-09-19 12:13:01 -0700

    Integrate Devextreme map demo

--- src/index.js
+++ src/index.js
@@ -18,4 +18,19 @@ app.get('/styles.css', function (req, res) {
   res.sendFile(__dirname + '/view/styles.css');
 });
 
+app.use('/devextreme/map-demo', express.static('./view/devextreme/map-demo'));
+
+app.use(
+  '/devextreme/getting-started',
+  express.static('./view/devextreme/getting-started')
+);
+
+app.get('/devextreme/map-demo', function (req, res) {
+  res.sendFile(__dirname + './view/devextreme/map-demo/index.html');
+});
+
+app.get('/devextreme/getting-started', function (req, res) {
+  res.sendFile(__dirname + './view/devextreme/getting-started/index.html');
+});
+
 app.listen(3000);
